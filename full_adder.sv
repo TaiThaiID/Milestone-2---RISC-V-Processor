b78d85844a2842e49f4b75c293673d064fd9272f
DThien111 <123047643+DThien111@users.noreply.github.com>
2025-10-10 22:15:42 +0700

    Deleted

--- full_adder.sv
+++ full_adder.sv
@@ -1,10 +1,1 @@
-module full_adder (
-    input  logic a,
-    input  logic b,
-    input  logic c_in,
-    output logic s,
-    output logic c_out
-);
-    assign s     = a ^ b ^ c_in;
-    assign c_out = (a & b) | (b & c_in) | (a & c_in);
-endmodule
+
